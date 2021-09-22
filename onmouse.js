2a02f2404ff368c54dc6e0b738b133f8736a2733
nasty23-star <71438930+nasty23-star@users.noreply.github.com>
2021-09-22 09:41:11 +0300

    Add files via upload

---
+++ onmouse.js
@@ -0,0 +1,6 @@
+function onmouseOver() {
+    document.getElementById("effect").style.opacity = "0.9";
+  }
+  function onmouseOut() {
+    document.getElementById("effect").style.opacity = "0.1";
+  }
